4da3fabbb413d852dce89e61b0c25d724104ffdc
Anna Catherine Espiritu Gomez <acgomez.dev@gmail.com>
2024-06-03 13:33:59 +0200

    Test finish - XmlHTTPReqiest Version

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
-const URL_TEST = []
+const URL_TEST = "https://api.jsonbin.io/v3/b/66573414ad19ca34f871177c"
 
-let searchDemand = () => {
+document.addEventListener("DOMContentLoaded", () => {
 
-}
+    let demandInfoContainer = document.querySelector(".demand-container")
+    let searchButton = document.querySelector(".search-button")
+    let searchMessage = document.querySelector(".search-message")
+    let tableBodyContainer = document.querySelector(".table-body")
 
-let writeResult = () => {
+    const searchDemand = (demandId) => {
+        let xhr
+        let datas
+        
+        xhr = new XMLHttpRequest()
 
-}
+        xhr.open('GET', URL_TEST)
+
+        xhr.onerror = () => {
+            searchButton.insertAdjacentHTML('afterend', '<span class="error-message">Page not found</span>')
+        }
+
+        xhr.onloadend = () => {
+            datas = JSON.parse(xhr.responseText)
+
+            if( xhr.status >= 200 && xhr.status < 400) {
+                console.log('Getting all datas went well...')
+
+                if(demandId > 0 && demandId <= datas.record.length) {
+                    searchMessage.innerHTML = ''
+                    writeResult(demandId, datas)
+                }
+                else {
+                    searchMessage.innerHTML = 
+                    `Please enter an ID between 1 and ${datas.record.length}`
+                    tableBodyContainer.insertAdjacentHTML('afterbegin', '<tr><th>Error</th></tr>')
+                } 
+            }
+            else {
+                console.log('Some error occured...')
+                searchButton.insertAdjacentHTML('afterend', '<span class="error-message">Page not found</span>')
+            }
+        }
+
+        xhr.send() 
+    }
+
+    let writeResult = (demandId, datas) => {
+        let resId
+        let id = demandId - 1
+
+        resId = `<tr>
+                    <th>${datas.record[id].demandId}</th>
+                    <th>${datas.record[id].firstName}</th>
+                    <th>${datas.record[id].lastName}</th>
+                    <th>${datas.record[id].registration}</th>
+                </tr>`
+        tableBodyContainer.insertAdjacentHTML('afterbegin', resId)
+    }
+
+    searchButton.addEventListener("click", () => {
+        let demandId = document.getElementById("search-id").value
+        searchDemand(demandId)
+    })    
+})
+
